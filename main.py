577d7d36e4185981582e17412a0153292fdb41af
Giacomo Belocchi <34477338+gbelocchi@users.noreply.github.com>
2025-06-06 19:08:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# app.py
 from flask import Flask, request
 from datetime import datetime
 
@@ -6,6 +7,9 @@ app = Flask(__name__)
 @app.route("/webprobe")
 def webprobe():
     ip = request.remote_addr
-    ua = request.headers.get("User-Agent")
+    ua = request.headers.get("User-Agent", "N/A")
     print(f"{datetime.now()} | IP: {ip} | UA: {ua}")
     return "ok", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
